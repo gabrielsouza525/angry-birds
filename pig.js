b631b84039f8d0fbe06987c23627f284b6cbd75c
Gabriel Souza <gabrielsouzaatc@gmail.com>
2021-12-21 19:56:58 -0300

    removemos o porco quando o atingimos em alta velocidade

--- pig.js
+++ pig.js
@@ -3,6 +3,23 @@ class Pig extends Base {
       super(x, y, 50, 50);
   
       this.imagem = loadImage("sprites/enemy.png");
+      this.visibilidade=255
+    }
+
+    display(){
+      console.log(this.corpo.speed)
+     if (this.corpo.speed<3) {
+      super.display()
+     } else {
+       World.remove(world,this.corpo)
+       push()
+       this.visibilidade=this.visibilidade-5
+       tint(255,this.visibilidade)
+       image(this.imagem,this.corpo.position.x,this.corpo.position.y,50,50)
+       pop()
+     } 
+      
+      
     }
   }
   
