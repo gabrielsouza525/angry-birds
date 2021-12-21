91dd539cb7042c63a177dc7f19d0602d14b0d177
Gabriel Souza <gabrielsouzaatc@gmail.com>
2021-12-21 19:27:27 -0300

    Eu fiz a segunda linha e add a cadeira e alterei a grossura do elastico

--- estilingue.js
+++ estilingue.js
@@ -16,13 +16,23 @@ class Estilingue{
     }
     display(){
         image(this.estilingue1, 200, 20);
-        image(this.estilingue2, 170, 20);
+        image(this.estilingue2, 170, 20);   
         if(this.restricao.bodyA){
             var pontoA=this.restricao.bodyA.position
             var pontoB=this.restricao.pointB
-            strokeWeight(4)
+            push()
             stroke(48,22,8)
+            if (pontoA.x<240 && pontoA.x>160) {
+                strokeWeight(7)     
+            } else {
+                strokeWeight(3)     
+            }
+            
             line(pontoA.x-20,pontoA.y,pontoB.x-10,pontoB.y)
+            line(pontoA.x-20,pontoA.y,pontoB.x+30,pontoB.y-3)
+            image(this.estilingue3, pontoA.x-30, pontoA.y-10,15,30)
+
+            pop()
         }
         
     }
